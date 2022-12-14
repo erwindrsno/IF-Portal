853a1c512d7a965fee92fbd03ee5afa632f1bbd8
Erwin Darsono <6182001002@student.unpar.ac.id>
2022-12-14 07:56:53 +0700

    inisialisasi project

---
+++ TuBes02/app/src/main/java/com/example/tubes_02/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tubes_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tubes_02.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
+    }
+}
