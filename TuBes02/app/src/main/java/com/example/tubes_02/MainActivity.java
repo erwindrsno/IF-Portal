1310ac7afc08f02efe4fa21f7e0fcc3465216a01
GerryNathanaelHersan <6182001037@student.unpar.ac.id>
2022-12-28 17:59:49 +0700

    New Pengumuman Activity

--- TuBes02/app/src/main/java/com/example/tubes_02/MainActivity.java
+++ TuBes02/app/src/main/java/com/example/tubes_02/MainActivity.java
@@ -13,58 +13,12 @@ import com.example.tubes_02.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private PengumumanFragment pengumumanFragment;
-    private PertemuanFragment pertemuanFragment;
-    private PrasyaratFragment prasyaratFragment;
-    private FragmentManager fragmentManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(this.binding.getRoot());
-        this.pengumumanFragment = new PengumumanFragment();
-        this.pertemuanFragment = new PertemuanFragment();
-        this.prasyaratFragment = new PrasyaratFragment();
 
-        this.fragmentManager = this.getSupportFragmentManager();
-
-        FragmentTransaction ft = this.fragmentManager.beginTransaction();
-        ft.add(this.binding.fragmentContainer.getId(), this.pengumumanFragment)
-                .addToBackStack(null)
-                .commit();
-
-        this.getSupportFragmentManager().setFragmentResultListener(
-                "changePage", this, new FragmentResultListener() {
-                    @Override
-                    public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
-                        String page = result.getString("page");
-                        changePage(page);
-                    }
-                }
-        );
     }
-    public void changePage(String page){
-        FragmentTransaction ft = this.fragmentManager.beginTransaction();
-//                .setCustomAnimations(
-//                        R.anim.fade_in,
-//                        R.anim.fade_out
-//                );
 
-        if(page.equals("pertemuan")){
-
-            ft.replace(binding.fragmentContainer.getId(),this.pertemuanFragment)
-                    .addToBackStack(null);
-        }
-        if(page.equals("pengumuman")){
-            ft.replace(binding.fragmentContainer.getId(),this.pengumumanFragment)
-                    .addToBackStack(null);
-        }
-        if(page.equals("prasyarat")){
-            ft.replace(binding.fragmentContainer.getId(),this.prasyaratFragment)
-                    .addToBackStack(null);
-        }
-
-        ft.commit();
-    }
 }
