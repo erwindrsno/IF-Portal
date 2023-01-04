56a70cea1c16b4f253ed6198e4b0c671c6b2ff44
Erwin Darsono <6182001002@student.unpar.ac.id>
2023-01-04 08:34:19 +0700

    dialog exit

---
+++ TuBes02/app/src/main/java/com/example/tubes_02/ExitAppDialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.tubes_02;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class ExitAppDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Override
+    public void onCancel(@NonNull DialogInterface dialog) {
+        super.onCancel(dialog);
+    }
+}
