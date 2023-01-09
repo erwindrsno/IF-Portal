6d565cde371bc3a9d556671e6a90f8a004bbf885
GerryNathanaelHersan <6182001037@student.unpar.ac.id>
2023-01-09 16:16:19 +0700

    buat homeadmin

---
+++ TuBes02/app/src/main/java/FiturAdmin/HomeAdminFragment.java
@@ -0,0 +1,15 @@
+package FiturAdmin;
+
+import androidx.fragment.app.Fragment;
+
+
+public class HomeAdminFragment extends Fragment {
+    public HomeAdminFragment(){
+
+    }
+
+    public static HomeAdminFragment newInstance() {
+        HomeAdminFragment fragment = new HomeAdminFragment();
+        return fragment;
+    }
+}
